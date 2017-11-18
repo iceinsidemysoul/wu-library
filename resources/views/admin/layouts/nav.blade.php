de0032d6f75c6c76cded92eb5b561f944b9cf7a2
Tanpisit Pitiphonsakun <sompongp.dev@gmail.com>
2017-11-18 18:03:08 +0700

    add link from admin to public index

--- resources/views/admin/layouts/nav.blade.php
+++ resources/views/admin/layouts/nav.blade.php
@@ -2,7 +2,7 @@
   <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#mainNavBar" aria-controls="mainNavBar" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="/admin">WU</a>
+  <a class="navbar-brand" href="/" target="_blank">WU</a>
 
   <div class="collapse navbar-collapse ml-md-5" id="mainNavBar">
     <ul class="navbar-nav mr-auto">
