a5afed4bf2dfb4cd9a8d81736dc94a39fc745956
iceinsidemysoul <sompong.paramon@gmail.com>
2017-11-19 13:06:51 +0700

    done footer, next social api

--- resources/views/spa-layouts/footer.blade.php
+++ resources/views/spa-layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <div style="margin-top:40px;"></div>
-<div class="wu-footer" style="background: #522b6f; border-top: 4px solid #1e1229; position:relative;bottom:0;right:0;">
+<div class="wu-footer pb-3" style="background: #522b6f; border-top: 4px solid #1e1229; position:relative;bottom:0;right:0;">
 	<div class="row mx-0">
-		<div class="col-10 offset-2 col-xl-10 offset-xl-1">
+		<div class="col-10 offset-2 col-xl-11 offset-xl-1">
 			<div class="row">
 				<div class="col-4 col-lg-2" style="max-width:120px">
 					<img src="/storage/images/wu-footer-logo.png" alt="" class="img-fluid">
@@ -17,7 +17,7 @@
 							  </li>
 							  <li class="nav-item">
 							    <a class="nav-link" style="color:#ff6a00" href="http://clm.wu.ac.th/" target="_blank">
-							    	<i class="fa fa-book"></i> ศูณย์บรรณสาร และสื่อการศึกษา
+							    	<i class="fa fa-book"></i> ศูนย์บรรณสารและสื่อการศึกษา
 							    </a>
 							  </li>
 							</ul>
@@ -25,12 +25,12 @@
 					</div>
 					<div class="row">
 						<div class="col">
-							<div class="text-white pb-2">
+							<div class="text-white pl-3">
 								<span class="text-center">
 									งานจดหมายเหตุและคลังสารสนเทศอิเล็กทรอนิกส์ ฝ่ายพัฒนาทรัพยากรสารสนเทศ ศูนย์บรรณสารและสื่อการศึกษา มหาวิทยาลัยวลัยลักษณ์
 								</span>
 								<br>
-								<span class="text-left">	
+								<span class="text-left pt-4">	
 									<i class="fa fa-phone text-wu-o"></i> 075-673364 
 									<i class="fa fa-envelope-o text-wu-o"></i> : nassara.ja@wu.ac.th
 								</span>
