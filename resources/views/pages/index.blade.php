8a902bc40a9cadb2576f124eb6c735b9a1c25159
iceinsidemysoul <sompong.paramon@gmail.com>
2017-11-22 07:22:21 +0000

    edit og:description v3

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
 @section ('og')
 	<meta property="twitter:card" content="article" />
 	<meta property="twitter:title" content="Walailak University Timeline - Archives" />
-	<meta property="twitter:description" content='ศ.ดร.วิจิตร ศรีสอ้าน ได้ชี้แจงว่า"...หากจะให้ได้มหาวิทยาลัยที่ดีที่สุดต้องใจเย็น ๆ และมหาวิทยาลัยที่นครศรีธรรมราชจะเป็นมหาวิทยาลัยที่สมบูรณ์แบบแนวใหม่ที่สุด..."' />
+	<meta property="twitter:description" content='พ.ศ.2533 ศ.ดร.วิจิตร ศรีสอ้าน ได้ชี้แจงว่า"...หากจะให้ได้มหาวิทยาลัยที่ดีที่สุดต้องใจเย็น ๆ และมหาวิทยาลัยที่นครศรีธรรมราชจะเป็นมหาวิทยาลัยที่สมบูรณ์แบบแนวใหม่ที่สุด..."' />
 	<meta property="twitter:image" content="http://172.104.165.212/storage/images/upload/1511170140-1134-forum.jpeg" />
 
 	<meta itemprop="name" content="Walailak University Timeline - Archives">
@@ -15,7 +15,7 @@
 	<meta property="og:url" content="{{ url('') }}" />
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content="Walailak University Timeline - Archives" />
-	<meta property="og:description" content='ศ.ดร.วิจิตร ศรีสอ้าน ได้ชี้แจงว่า"...หากจะให้ได้มหาวิทยาลัยที่ดีที่สุดต้องใจเย็น ๆ และมหาวิทยาลัยที่นครศรีธรรมราชจะเป็นมหาวิทยาลัยที่สมบูรณ์แบบแนวใหม่ที่สุด..."' />
+	<meta property="og:description" content='พ.ศ.2533 ศ.ดร.วิจิตร ศรีสอ้าน ได้ชี้แจงว่า"...หากจะให้ได้มหาวิทยาลัยที่ดีที่สุดต้องใจเย็น ๆ และมหาวิทยาลัยที่นครศรีธรรมราชจะเป็นมหาวิทยาลัยที่สมบูรณ์แบบแนวใหม่ที่สุด..."' />
 	<meta property="og:image" content="http://172.104.165.212/storage/images/upload/1511170140-1134-forum.jpeg" />
 
 @endsection
